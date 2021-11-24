e68a3bc9afb55fdd75567255e66ca0206143e881
Waleed Abdulla <waleed18574@hotmail.com>
2021-11-24 13:29:37 +0200

    Add files via upload

---
+++ MS_SQL_Server_project/SQL_Server_Project_2_Querying_Parch_&_Posey_Database/Project_documentation/Etablishing and Populating DB_BUSINESS_REQUESTS.sql
@@ -0,0 +1,41 @@
+USE master
+GO
+
+DROP DATABASE IF EXISTS [Business_Requests]
+GO
+
+CREATE DATABASE [Business_Requests]
+GO
+
+USE [Business_Requests]
+GO
+
+
+
+CREATE TABLE tbl_business_requests(
+							   id SMALLINT
+							   ,date DATE
+							   ,department VARCHAR(30)
+							   ,question_id TINYINT
+							   ,question VARCHAR(1000)
+							   ,answer VARCHAR(2000) null
+							   )
+
+
+
+
+  BULK INSERT TBL_BUSINESS_REQUESTS
+ FROM 'C:\Users\ADMIN\Desktop\CSV Files\Documentation Files\03- Business Requests Database-Sales_&_Marketing_Business_Requests.csv'
+ WITH(
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	ROWTERMINATOR = '\n'
+	 )
+ GO
+
+
+
+ SELECT * FROM TBL_BUSINESS_REQUESTS
+ GO
+
+
